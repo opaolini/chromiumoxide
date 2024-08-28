0b1d44cbef57e6ea6193e4053da7fdea58d383ea
opaolini <oskar@paolini.pl>
2024-08-28 10:10:51 +0200

    use an interim value for arbitrary precision issues

--- src/conn.rs
+++ src/conn.rs
@@ -10,7 +10,9 @@ use futures::task::{Context, Poll};
 use futures::{SinkExt, StreamExt};
 
 use chromiumoxide_cdp::cdp::browser_protocol::target::SessionId;
-use chromiumoxide_types::{CallId, EventMessage, Message, MethodCall, MethodId};
+use chromiumoxide_types::{
+    CallId, CdpJsonEventMessage, EventMessage, Message, MethodCall, MethodId,
+};
 
 use crate::error::CdpError;
 use crate::error::Result;
@@ -148,9 +150,15 @@ impl<T: EventMessage + Unpin> Stream for Connection<T> {
                     Err(err) => {
                         tracing::debug!(target: "chromiumoxide::conn::raw_ws::parse_errors", msg = text, "Failed to parse raw WS message");
                         tracing::error!("Failed to deserialize WS response {}", err);
-                        Err(err.into())
+                        let interim: serde_json::Value = serde_json::from_str(&text)?;
+                        let msg = serde_json::from_value::<Message<T>>(interim.clone());
+                        match msg {
+                            Ok(m) => Ok(m),
+                            Err(_e) => Err(err.into()),
+                        }
                     }
                 };
+
                 Poll::Ready(Some(ready))
             }
             Some(Ok(WsMessage::Close(_))) => Poll::Ready(None),
